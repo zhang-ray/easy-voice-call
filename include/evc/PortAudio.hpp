2d932080c0ecbe73291a879453b4814ff84c1d94
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-09-24 21:18:46 +0800

    use PortAudio directly

---
+++ include/evc/PortAudio.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+
+#include <vector>
+#include <string>
+
+#include "Singleton.hpp"
+#include "AudioDevice.hpp"
+
+#include <portaudio.h>
+#include <iostream>
+
+class PortAudio : public AudioDevice, public Singleton<PortAudio> {
+private:
+    PaStream *stream_ = nullptr;
+public:
+    virtual ReturnType init() override{
+        auto err = Pa_Initialize();
+        if( err != paNoError ){
+            return Pa_GetErrorText( err );
+        }
+
+        std::cout << __FUNCTION__ << ":" <<  __LINE__ << std::endl;
+        #define SAMPLE_RATE (48000)
+
+        /* Open an audio I/O stream. */
+        err = Pa_OpenDefaultStream(
+            &stream_,
+            1, 1,
+            paInt16, SAMPLE_RATE,
+            1<<7,
+            nullptr, nullptr);
+        if( err != paNoError ) {
+            // error
+            return err;
+        }
+
+        err = Pa_StartStream(stream_);
+        if( err != paNoError ) {
+            // error
+            return err;
+        }
+        return 0;
+    }
+    virtual int read(char *pBuffer, int nBytes) override {
+        auto err = Pa_ReadStream(stream_, (void*)pBuffer, nBytes);
+        if (err != paNoError){
+            std::cout << Pa_GetErrorText( err ) << std::endl;
+            return err;
+        }
+        return nBytes;
+    }
+    virtual int write(const char *pBuffer, int nBytes) override {
+        auto err = Pa_WriteStream(stream_, (void*)pBuffer, nBytes);
+        if (err != paNoError){
+            std::cout << Pa_GetErrorText( err ) << std::endl;
+            return err;
+        }
+        return nBytes;
+    }
+
+    ~PortAudio(){
+        auto err = Pa_StopStream(stream_);
+        if( err != paNoError ){
+            //printf(  "PortAudio error: %s\n", Pa_GetErrorText( err ) );
+        }
+        err = Pa_Terminate();
+        if( err != paNoError ){
+            //printf(  "PortAudio error: %s\n", Pa_GetErrorText( err ) );
+        }
+    }
+
+};
--- src/Factory.cpp
+++ src/Factory.cpp
@@ -1,7 +1,16 @@
 #include "evc/Factory.hpp"
+#if 0
 #include "evc/Alsa.hpp"
 
 
 AudioDevice &Factory::create() {
     return (AudioDevice &)(Alsa::get());
+}
+
+#endif
+
+// use PortAudio backend
+#include "evc/PortAudio.hpp"
+AudioDevice &Factory::create() {
+    return (AudioDevice &)(PortAudio::get());
 }
