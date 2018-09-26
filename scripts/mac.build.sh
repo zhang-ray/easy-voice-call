9b5efb567bda66e402535aa3c843969abecb2bef
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-09-26 23:13:12 +0800

    Update mac.build.sh

--- scripts/mac.build.sh
+++ scripts/mac.build.sh
@@ -1,8 +1,14 @@
+
+# opus-1.2.1, same as Ubuntu 16.04
 curl -O https://archive.mozilla.org/pub/opus/opus-1.2.1.tar.gz
 tar -zxf opus-1.1.2.tar.gz
 ./configure
 make -j32
 make install
+
+# portaudio
+brew install portaudio
+
 
 mkdir ../easy-voice-call-build
 cd ../easy-voice-call-build
