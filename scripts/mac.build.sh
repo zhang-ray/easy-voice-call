28c1e7174792865362d97423659516001ff1c4fc
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-09-26 23:06:44 +0800

    Update mac.build.sh

--- scripts/mac.build.sh
+++ scripts/mac.build.sh
@@ -1,2 +1,10 @@
-# https://github.com/mstorsjo/fdk-aac
-brew install fdk-aac
+curl -O https://archive.mozilla.org/pub/opus/opus-1.2.1.tar.gz
+tar -zxf opus-1.1.2.tar.gz
+./configure
+make -j32
+make install
+
+mkdir ../easy-voice-call-build
+cd ../easy-voice-call-build
+cmake ../easy-voice-call
+make -j3
