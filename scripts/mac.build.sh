ce88b29f03050ff53e2e3371fad99cd9d8094c5e
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-10-12 11:25:52 +0800

    [macOS] make .app artifact

--- scripts/mac.build.sh
+++ scripts/mac.build.sh
@@ -22,6 +22,13 @@ cmake ../easy-voice-call
 make -j3
 
 
-#which macdeployqt
-mdfind -name macdeployqt
-macdeployqt client_qt5
+
+
+
+####### make artifact
+# TODO: try to auto find the macdeployqt path?
+/usr/local/Cellar/qt/5.11.2/bin/macdeployqt client_qt5
+mkdir client_qt5/Contents/MacOS
+cp client_qt5/client_qt5 client_qt5/Contents/MacOS
+mv client_qt5 client_qt5.app
+zip -r client_qt5.app.zip client_qt5.app
