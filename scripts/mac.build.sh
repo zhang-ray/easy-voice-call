f955be06606bb5b5d7e519f656701bcd792cfd6a
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-10-13 17:45:24 +0800

    [macOS] clean some useless artifacts

--- scripts/mac.build.sh
+++ scripts/mac.build.sh
@@ -29,6 +29,10 @@ make -j3
 # TODO: try to auto find the macdeployqt path?
 /usr/local/Cellar/qt/5.11.2/bin/macdeployqt client_qt5
 mkdir client_qt5/Contents/MacOS
-cp client_qt5/client_qt5 client_qt5/Contents/MacOS
+mv client_qt5/client_qt5 client_qt5/Contents/MacOS
+rm -rf CMakeFiles
+rm -f Makefile
+rm -rf client_qt5_autogen
+rm cmake_install.cmake
 mv client_qt5 client_qt5.app
 zip -r client_qt5.app.zip client_qt5.app
