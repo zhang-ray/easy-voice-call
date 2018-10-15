65661abc488a00f7240b9249b9268dc41ee1b275
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-10-16 00:04:25 +0800

    [CI/CD] +make Server.AppImage

--- scripts/ubuntu.build.sh
+++ scripts/ubuntu.build.sh
@@ -7,20 +7,39 @@ cmake ../easy-voice-call
 make -j3
 
 
-###################### make AppImage ###################### 
-pwd
+###################### prepare AppImage ###################### 
+wget "https://github.com/AppImage/AppImageKit/releases/download/10/AppRun-x86_64" #  -O AppDir/AppRun 
+wget "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
+wget "https://github.com/AppImage/AppImageKit/releases/download/10/appimagetool-x86_64.AppImage"
+chmod a+x AppRun-x86_64
+chmod a+x linuxdeploy-x86_64.AppImage
+chmod a+x appimagetool-x86_64.AppImage
+
+
+###################### make GUI client AppImage ###################### 
+cd ./client_qt5/
 mkdir AppDir
 mkdir AppDir/usr/
 mkdir AppDir/usr/bin/
-wget "https://github.com/AppImage/AppImageKit/releases/download/10/AppRun-x86_64" -O AppDir/AppRun 
-chmod a+x AppDir/AppRun
-cp ./client_qt5/client_qt5 AppDir/usr/bin/EasyVoiceCall.qt5
+cp ../AppRun-x86_64 AppDir/AppRun
+cp ./client_qt5 AppDir/usr/bin/EasyVoiceCall.qt5
 echo -e "[Desktop Entry]\nName=EasyVoiceCall.qt5\nExec=EasyVoiceCall.qt5\nIcon=EasyVoiceCall.qt5\nType=Application" > AppDir/EasyVoiceCall.qt5.desktop
 convert -size 256x256 xc:transparent AppDir/EasyVoiceCall.qt5.png
-wget "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
-chmod a+x linuxdeploy-x86_64.AppImage
-./linuxdeploy-x86_64.AppImage --appdir=AppDir
-wget "https://github.com/AppImage/AppImageKit/releases/download/10/appimagetool-x86_64.AppImage"
-chmod a+x appimagetool-x86_64.AppImage
-./appimagetool-x86_64.AppImage AppDir
-mv EasyVoiceCall.qt5-x86_64.AppImage ../
+../linuxdeploy-x86_64.AppImage --appdir=AppDir
+../appimagetool-x86_64.AppImage AppDir
+mv EasyVoiceCall.qt5-x86_64.AppImage ../../
+
+
+
+###################### make server AppImage ###################### 
+cd ./server/
+mkdir AppDir
+mkdir AppDir/usr/
+mkdir AppDir/usr/bin/
+cp ../AppRun-x86_64 AppDir/AppRun
+cp ./server AppDir/usr/bin/EasyVoiceCall.Server
+echo -e "[Desktop Entry]\nName=EasyVoiceCall.Server\nExec=EasyVoiceCall.Server\nIcon=EasyVoiceCall.Server\nType=Application" > AppDir/EasyVoiceCall.Server.desktop
+convert -size 256x256 xc:transparent AppDir/EasyVoiceCall.Server.png
+../linuxdeploy-x86_64.AppImage --appdir=AppDir
+../appimagetool-x86_64.AppImage AppDir
+mv EasyVoiceCall.Server-x86_64.AppImage ../../
