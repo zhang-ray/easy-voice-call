c154f77986cc7dafdd1e1abc13973b132f76fdbd
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-10-15 20:21:30 +0800

    [linux] +AppImage

--- scripts/ubuntu.build.sh
+++ scripts/ubuntu.build.sh
@@ -4,3 +4,21 @@ mkdir ../easy-voice-call-build
 cd ../easy-voice-call-build
 cmake ../easy-voice-call
 make -j3
+
+
+###################### make AppImage ###################### 
+pwd
+mkdir AppDir
+mkdir AppDir/usr/
+mkdir AppDir/usr/bin/
+wget "https://github.com/AppImage/AppImageKit/releases/download/10/AppRun-x86_64" -O AppDir/AppRun 
+chmod a+x AppDir/AppRun
+cp ./client_qt5/client_qt5 AppDir/usr/bin/EasyVoiceCall.qt5
+echo -e "[Desktop Entry]\nName=EasyVoiceCall.qt5\nExec=EasyVoiceCall.qt5\nIcon=EasyVoiceCall.qt5\nType=Application" > AppDir/EasyVoiceCall.qt5.desktop
+convert -size 256x256 xc:transparent AppDir/EasyVoiceCall.qt5.png
+wget "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
+chmod a+x linuxdeploy-x86_64.AppImage
+./linuxdeploy-x86_64.AppImage --appdir=AppDir
+wget "https://github.com/AppImage/AppImageKit/releases/download/10/appimagetool-x86_64.AppImage"
+chmod a+x appimagetool-x86_64.AppImage
+./appimagetool-x86_64.AppImage AppDir
