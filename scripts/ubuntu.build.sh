9750632f717d45fed73d2dd03570e24fb7f51fa9
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-10-16 14:23:00 +0800

    [linux] try to fix #3: The *.desktop file is lacking a Categories= entry

--- scripts/ubuntu.build.sh
+++ scripts/ubuntu.build.sh
@@ -23,7 +23,7 @@ mkdir AppDir/usr/
 mkdir AppDir/usr/bin/
 cp ../AppRun-x86_64 AppDir/AppRun
 cp ./client_qt5 AppDir/usr/bin/EasyVoiceCall.qt5
-echo -e "[Desktop Entry]\nName=EasyVoiceCall.qt5\nExec=EasyVoiceCall.qt5\nIcon=EasyVoiceCall.qt5\nType=Application" > AppDir/EasyVoiceCall.qt5.desktop
+echo -e "[Desktop Entry]\nName=EasyVoiceCall.qt5\nExec=EasyVoiceCall.qt5\nIcon=EasyVoiceCall.qt5\nType=Application\nCategories=AudioVideo;Audio;" > AppDir/EasyVoiceCall.qt5.desktop
 convert -size 256x256 xc:transparent AppDir/EasyVoiceCall.qt5.png
 ../linuxdeploy-x86_64.AppImage --appdir=AppDir
 # try to fix:
@@ -44,7 +44,7 @@ mkdir AppDir/usr/
 mkdir AppDir/usr/bin/
 cp ../AppRun-x86_64 AppDir/AppRun
 cp ./server AppDir/usr/bin/EasyVoiceCall.Server
-echo -e "[Desktop Entry]\nName=EasyVoiceCall.Server\nExec=EasyVoiceCall.Server\nIcon=EasyVoiceCall.Server\nType=Application" > AppDir/EasyVoiceCall.Server.desktop
+echo -e "[Desktop Entry]\nName=EasyVoiceCall.Server\nExec=EasyVoiceCall.Server\nIcon=EasyVoiceCall.Server\nType=Application\nCategories=AudioVideo;Audio;" > AppDir/EasyVoiceCall.Server.desktop
 convert -size 256x256 xc:transparent AppDir/EasyVoiceCall.Server.png
 ../linuxdeploy-x86_64.AppImage --appdir=AppDir
 ../appimagetool-x86_64.AppImage AppDir
