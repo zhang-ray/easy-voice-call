a12e15dac5eefb6e32ffb2a4de0e2b0a637d340c
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-11-06 10:00:36 +0800

    try to fix boost link issue

--- scripts/ubuntu.build.sh
+++ scripts/ubuntu.build.sh
@@ -16,26 +16,6 @@ chmod a+x linuxdeploy-x86_64.AppImage
 chmod a+x appimagetool-x86_64.AppImage
 
 
-###################### make GUI client AppImage ###################### 
-cd ./client_qt5/
-mkdir AppDir
-mkdir AppDir/usr/
-mkdir AppDir/usr/bin/
-cp ../AppRun-x86_64 AppDir/AppRun
-cp ./client_qt5 AppDir/usr/bin/EasyVoiceCall.Ubuntu.14.04
-echo -e "[Desktop Entry]\nName=EasyVoiceCall.Ubuntu.14.04\nExec=EasyVoiceCall.Ubuntu.14.04\nIcon=EasyVoiceCall.Ubuntu.14.04\nType=Application\nCategories=AudioVideo;Audio;" > AppDir/EasyVoiceCall.Ubuntu.14.04.desktop
-convert -size 256x256 xc:transparent AppDir/EasyVoiceCall.Ubuntu.14.04.png
-../linuxdeploy-x86_64.AppImage --appdir=AppDir
-# try to fix:
-# ``` This application failed to start because it could not find or load the Qt platform plugin "xcb".
-mkdir AppDir/usr/lib/x86_64-linux-gnu
-mkdir AppDir/usr/lib/x86_64-linux-gnu/qt5
-mkdir AppDir/usr/lib/x86_64-linux-gnu/qt5/plugins
-cp -r /usr/lib/x86_64-linux-gnu/qt5/plugins/platforms AppDir/usr/lib/x86_64-linux-gnu/qt5/plugins
-../appimagetool-x86_64.AppImage AppDir
-mv EasyVoiceCall.Ubuntu.14.04-x86_64.AppImage ../../
-cd ..
-
 
 ###################### make server AppImage ###################### 
 cd ./server/
