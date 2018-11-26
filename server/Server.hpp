5520e7ee6bee6ea373f2ad7258590a05b5028a2f
Ray Zhang <41682958+zhang-ray@users.noreply.github.com>
2018-11-26 11:39:38 +0800

    [protocol] rename server files

--- server/Server.hpp
+++ server/Server.hpp
@@ -11,6 +11,7 @@
 #include "ReturnType.hpp"
 #include "ServerLogger.hpp"
 
+
 class Participant {
 public:
     virtual ~Participant() {}
@@ -261,58 +262,5 @@ private:
 void printUsage() {
     std::cerr << "Usage: EvcServer <port>\n";
     std::cerr << "   or: EvcServer <port> <broadcast/echo>\n";
-}
-
-
-int main(int argc, char* argv[]) {
-    try {
-        int port = 80;
-
-        // init port
-        {
-            if ((argc == 2) || (argc == 3)) {
-                if (argc == 3) {
-                    if (!strcmp("broadcast", argv[2])) {
-                        isEchoMode = false;
-                    }
-                    else if (!strcmp("echo", argv[2])) {
-                        isEchoMode = true;
-                    }
-                    else {
-                        printUsage();
-                        return -1;
-                    }
-                }
-                port = std::atoi(argv[1]);
-            }
-            else {
-                printUsage();
-                return -1;
-            }
-        }
-
-        BOOST_LOG_TRIVIAL(trace) << "PORT=" << port;
-
-
-        /// init logger
-        {
-            //boost::log::core::get()->set_filter(boost::log::trivial::severity >= boost::log::trivial::trace);
-        }
-
-        boost::asio::io_service io_service;
-        Server server(
-            io_service,
-            boost::asio::ip::tcp::endpoint(
-                boost::asio::ip::tcp::v4(),
-                port
-            )
-        );
-        io_service.run();
-    }
-    catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << "\n";
-    }
-
-    return 0;
 }
 
